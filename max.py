c4f007d4a7ccb758aaf84a6375b24f52ad382799
Ekaterina19 <gorskovaekaterina19@gmail.com>
2026-04-23 19:15:47 +0300

    mod: Убрала переменную maxsimum

--- max.py
+++ max.py
@@ -2,8 +2,7 @@ num1 = int(input("Введите первое целое число: "))
 num2 = int(input("Введите второе целое число: "))
 
 if num1 > num2:
-    maximum = num1
+    print(f"Максимальное из чисел {num1} и {num2} равно: {num1}")
 else:
-    maximum = num2
+    print(f"Максимальное из чисел {num1} и {num2} равно: {num2}")
 
-print(f"Максимальное из чисел {num1} и {num2} равно: {maximum}")
