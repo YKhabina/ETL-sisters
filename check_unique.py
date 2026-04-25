901441f6bd87759b09638a406ef9c10d2500bf32
ykhabina <jsb24@mail.ru>
2026-04-25 23:25:31 +0500

    Добавила символы

--- check_unique.py
+++ check_unique.py
@@ -1,6 +1,6 @@
 # put your python code here
 input_str = input("Введите последовательность символов")
-if len(input_str) == len(set(input_str)):
+if len(input_str) == len(set(input_str&&&&&&&&&&&)):
     print(f"Строка {input_str} имеет уникальные символы")
 else: 
     print(f"К сожалению, в строке {input_str}  повторяются символы")
