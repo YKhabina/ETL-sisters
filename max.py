7a5e9827d663fe84d1fbb619e3150f43817d9140
ykhabina <jsb24@mail.ru>
2026-04-25 23:03:57 +0500

    Revert "mod: Убрала переменную maxsimum"
    This reverts commit c4f007d4a7ccb758aaf84a6375b24f52ad382799.

--- max.py
+++ max.py
@@ -2,7 +2,8 @@ num1 = int(input("Введите первое целое число: "))
 num2 = int(input("Введите второе целое число: "))
 
 if num1 > num2:
-    print(f"Максимальное из чисел {num1} и {num2} равно: {num1}")
+    maximum = num1
 else:
-    print(f"Максимальное из чисел {num1} и {num2} равно: {num2}")
+    maximum = num2
 
+print(f"Максимальное из чисел {num1} и {num2} равно: {maximum}")
